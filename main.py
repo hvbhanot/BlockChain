6326e9f3f59251af83851ba62640b1adbb60fcd8
hvbhanot17 <hvbhanot1@gmail.com>
2024-06-19 01:58:54 -0500

    v1

--- main.py
+++ main.py
@@ -1,1 +1,38 @@
-print("Hello BlockChain")
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify
+
+
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof=1, prev_hash='0')
+
+    def create_block(self, proof, prev_hash):
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': str(datetime.datetime.now()),
+            'proof': proof,
+            'prev_hash': prev_hash
+        }
+        self.chain.append(block)
+        return block
+
+    def prev_block(self):
+        return self.chain[-1]
+
+    def proofOfWork(self, prev_proof):
+        newProof = 1
+        checkProof = False
+        while checkProof is False:
+            hash_operation = hashlib.sha256(str(newProof ** 2 - prev_proof ** 2).encode()).hexdigest()
+            if hash_operation[:4] == '0000':
+                checkProof = True
+            else:
+                newProof += 1
+
+        return newProof
+
+
+Blockchain
